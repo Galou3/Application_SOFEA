a27b29735922688f40dea20c0024d0f65052ebae
Galou3 <90267128+Galou3@users.noreply.github.com>
2023-03-20 15:25:01 +0100

    add cocktails list

--- front/src/components/CocktailItem.vue
+++ front/src/components/CocktailItem.vue
@@ -1,30 +1,13 @@
 <template>
   <div class="cocktail-item">
-    <img :src="cocktail.image" :alt="cocktail.name" class="cocktail-image" />
-    <div class="buttons">
-      <button class="like-button" @click="likeCocktail">Like</button>
-      <button class="dislike-button" @click="dislikeCocktail">Dislike</button>
-    </div>
+    <h3>{{ cocktail.name }}</h3>
+    <img :src="cocktail.url" :alt="cocktail.name" class="cocktail-image" />
+
   </div>
 </template>
+<script setup >
+defineProps(['cocktail'])
 
-<script>
-export default {
-  props: {
-    cocktail: {
-      type: Object,
-      required: true,
-    },
-  },
-  methods: {
-    likeCocktail() {
-      console.log("Cocktail Liked");
-    },
-    dislikeCocktail() {
-      console.log("Cocktail Disliked");
-    },
-  },
-};
 </script>
 
 
